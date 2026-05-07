3738edad831275462e3ef1b35b8f9c8888570153
Yaroslav Bondarchuk <bondarchuk.yaroslav@e-u.edu.ua>
2026-05-07 17:29:10 +0300

    Практична №7

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,51 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+pub fn breaking_records(scores: &[i32]) -> Vec<i32> {
+    if scores.is_empty() {
+        return vec![0, 0];
+    }
+
+    let mut min_score = scores[0];
+    let mut max_score = scores[0];
+    let mut min_count = 0;
+    let mut max_count = 0;
+
+    for &score in &scores[1..] {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
+        }
+    }
+
+    vec![max_count, min_count]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample_0() {
+        let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+        assert_eq!(breaking_records(&scores), vec![2, 4]);
+    }
+
+    #[test]
+    fn test_sample_1() {
+        let scores = vec![3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+        assert_eq!(breaking_records(&scores), vec![4, 0]);
+    }
+
+    #[test]
+    fn test_single_score() {
+        let scores = vec![10];
+        assert_eq!(breaking_records(&scores), vec![0, 0]);
+    }
+
+    #[test]
+    fn test_equal_scores() {
+        let scores = vec![10, 10, 10];
+        assert_eq!(breaking_records(&scores), vec![0, 0]);
+    }
+}
